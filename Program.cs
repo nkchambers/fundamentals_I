4afbd48bb21a2254b0fa15740dc219e217f4325b
nkchambers <nkchames@gmail.com>
2021-11-29 22:33:58 -0800

    completed all challenges

--- Program.cs
+++ Program.cs
@@ -6,10 +6,40 @@ namespace fundamentals_I
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 256; i++) {
+        //----------------------1st Challenge-------------------------
+            for (int i=1; i<256; i++) {
                 
                 Console.WriteLine(i);
 
+            }
+
+        //------------------------2nd Challenge-------------------------
+            for (int i=1; i<101; i++) {
+                if (i%3 == 0  &&  i%5 == 0) {
+                    continue;
+                }
+                else if (i%3 == 0  ||  i%5 == 0) {
+                    Console.WriteLine(i);
+                }
+                else {
+                    continue;
+                }
+            }
+
+        //------------------------3rd Challenge-------------------------
+            for (int i=1; i<101; i++) {
+                if (i%3 == 0  &&  i%5 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i%3 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+                else {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
