bd22e86f13a46fe61aec3b8ecac08b39467a6a8a
nkchambers <nkchames@gmail.com>
2021-11-29 21:59:07 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace fundamentals_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 256; i++) {
+                
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
